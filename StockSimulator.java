04420b4d29b7ec1750c66d721491e834e2cfdcd6
Bogusz Jelinski <bogusz.jelinski@gmail.com>
2019-11-26 22:33:33 +0100

    MAX optimization with Julia

--- StockSimulator.java
+++ StockSimulator.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -11,11 +13,14 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.correlation.Covariance;
 
 public class StockSimulator {
@@ -23,10 +28,16 @@ public class StockSimulator {
 	private static final String ROOT_DIR = "/home/dell/DIVID/";
 	private static final String SHARE_NAMES = ROOT_DIR + "GIT/SP100.txt";
 	private static final String PYTHON_CODE = ROOT_DIR + "mpt.py";
-	private static final String PYTHON_OUTPUT = ROOT_DIR + "py.out";
+	private static final String SOLVER_OUTPUT = ROOT_DIR + "solver.out";
+	private static final String JULIA_CODE = ROOT_DIR + "mpt.jl";
+	private static final String JULIA_CMD = "C:\\Users\\dell\\AppData\\Local\\Julia-0.5.2\\bin\\julia";
 	private static final String SIMUL_OUTPUT = ROOT_DIR + "simul.html";
 	private static final double MIN_SHARE = 0.0001; // minimal share of a stock we accept in a portfolio  
-	private static final double MIN_RISK = 0.035;
+	private static final double MIN_REVE = 0.035; // minimal revenue
+	private static final double MAX_RISK = 0.015; // minimal revenue
+	private static final int    MAX_STOCKS = 4; // how many stocks we will have in portfolio in "dividend arbitrage" (without solver)
+	private static final int 	MINIMIZE = 0;
+	private static final int 	MAXIMIZE = 1;
 	
 	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	private static SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
@@ -85,7 +96,7 @@ public class StockSimulator {
 			this.wallet = wallet;
 			this.portfolio = portfolio;
 		}
-		public double wallet;
+		public double wallet; // TODO: this should be BigDecimal
 		public List<StockInPortfolio> portfolio;
 	}
 	
@@ -130,17 +141,24 @@ public class StockSimulator {
 			// here we sell the stock virtually
 			wallet = wallet.add(stockWithDividend);
 			
-			PortfolioWithWallet stockAndWallet = findBestPortfolioWith_MPT(dateTo.minusMonths(risk_ret_periods*period_length),
-													period_length, risk_ret_periods, wallet, bw);
+			PortfolioWithWallet stockAndWallet = findBestPortfolioWith_MPT(MAXIMIZE,
+													dateTo.minusMonths(risk_ret_periods*period_length),
+													period_length, risk_ret_periods, wallet, MAX_RISK, bw);
+												/*findBestPortfolioWith_MPT(MINIMIZE,
+														dateTo.minusMonths(risk_ret_periods*period_length),
+														period_length, risk_ret_periods, wallet, MIN_REVE, bw);
+												findAndBuyBestPortfolioWith_DA1(
+													dateTo.minusMonths(risk_ret_periods*period_length),
+													period_length, risk_ret_periods, wallet, MAX_RISK, MAX_STOCKS, bw);*/
 			portfolio = stockAndWallet.portfolio;
 			wallet = new BigDecimal(stockAndWallet.wallet);
 		}
 		bw.close();
 	}
 	
-	private static PortfolioWithWallet findBestPortfolioWith_MPT(LocalDate startDate, 
+	private static PortfolioWithWallet findBestPortfolioWith_MPT(int type, LocalDate startDate, 
 													int period_length, int periods_number,
-													BigDecimal budget, BufferedWriter bw) throws IOException {
+													BigDecimal budget, double min_r, BufferedWriter bw) throws IOException {
 		double[][] benefits = new double[tickers.size()][periods_number]; 
 		String[] goodTickers = new String[tickers.size()];
 		Close[] lastPrice = new Close[tickers.size()];
@@ -156,11 +174,11 @@ public class StockSimulator {
 				LocalDate toDate   = startDate.plusMonths((i+1) * period_length);
 
 				List<Close> historicPricesTemp = historicPrices.stream() // limiting to the "risk assessment" period
-						.filter(p -> (p.date.isAfter(fromDate) && p.date.isBefore(toDate)) || p.date.isEqual(fromDate)) // stopDate belongs to the next period
+						.filter(p -> (p.date.isAfter(fromDate) && p.date.isBefore(toDate)) || p.date.isEqual(toDate)) // stopDate belongs to the next period
 						.collect(Collectors.toList()); // maybe sorting to secure against different order in source files ?
 				
 				List<Close> historicDividendsTemp = historicDividends.stream() // limiting to the "risk assessment" period
-						.filter(p -> (p.date.isAfter(fromDate) && p.date.isBefore(toDate)) || p.date.isEqual(fromDate))
+						.filter(p -> (p.date.isAfter(fromDate) && p.date.isBefore(toDate)) || p.date.isEqual(toDate))
 						.collect(Collectors.toList());
 				// checking price difference
 				if (historicPricesTemp == null || historicPricesTemp.size() == 0) break; // do not take this stock into consideration
@@ -174,6 +192,7 @@ public class StockSimulator {
 					for (Close div: historicDividendsTemp)
 						if (div.price != null)
 							sum = sum.add(div.price);
+				
 				benefits[idx][i] = last.price.subtract(first.price).add(sum).doubleValue();
 				benefits[idx][i] /= first.price.doubleValue();
 			}
@@ -196,12 +215,18 @@ public class StockSimulator {
 	      mean[i] /= periods_number;
 		}
 		double[][] cov = myCov(idx, periods_number, benef, mean); // TODO: double[][] cov = new Covariance(benef).getCovarianceMatrix().getData();
-		constructPythonTask(cov, mean, MIN_RISK);
-		runPython();
+		if (type == MINIMIZE) {
+			constructPythonTask(cov, mean, min_r);
+			runOsCmd("python " + PYTHON_CODE);
+		}
+		else { // MAXIMIZE
+			constructJuliaTask(cov, mean, min_r);
+			runJulia(JULIA_CMD + " "+ JULIA_CODE + " > julia.out 2>nul");
+		}
 		return createPortfolio(budget.doubleValue(), idx, goodTickers, lastPrice, bw);
 	}
 	
-	private static void constructPythonTask(double[][] cov, double[] mean, double min_risk) throws IOException {
+	private static void constructPythonTask(double[][] cov, double[] mean, double min_reve) throws IOException {
 		FileWriter fw = new FileWriter(PYTHON_CODE);    
         fw.write("from cvxopt import matrix\n");
         fw.write("from cvxopt.solvers import qp\n");
@@ -225,27 +250,142 @@ public class StockSimulator {
         	if (i<mean.length-1) fw.write(",");
         }
         fw.write("])\n");
-        fw.write("r_min = "+ min_risk + "\n"); //0.035
+        fw.write("r_min = "+ min_reve + "\n"); //0.035
         fw.write("G = matrix(np.concatenate((-np.transpose(Mean), -np.identity(n)), 0))\n");
         fw.write("h = matrix(np.concatenate((-np.ones((1,1))*r_min, np.zeros((n,1))), 0))\n");
         fw.write("A = matrix(1.0, (1,n))\n");
         fw.write("b = matrix(1.0)\n");
         fw.write("q = matrix(np.zeros((n, 1)))\n");
         fw.write("sol = qp(Cov, q, G, h, A, b)\n");
-        fw.write("with open(\""+PYTHON_OUTPUT+"\", \"w\") as f:\n");
+        fw.write("with open(\""+SOLVER_OUTPUT+"\", \"w\") as f:\n");
         fw.write("    for item in sol['x']:\n");
         fw.write("        f.write(\"%s\\n\" % item)\n");
         fw.close();
 	}
 	
-	private static void runPython() throws IOException {
-		 Process p = Runtime.getRuntime().exec("python " + PYTHON_CODE);
+	private static void constructJuliaTask(double[][] cov, double[] mean, double min_reve) throws IOException {
+		FileWriter fw = new FileWriter(JULIA_CODE);    
+        fw.write("using JuMP \n");
+        fw.write("using Ipopt \n");
+        fw.write("N="+ cov.length + " \n");
+        fw.write("m = Model(solver=IpoptSolver()) \n");
+        fw.write("C = [");
+        for (int i = 0; i < cov.length; i++) {
+        	for (int j =0; j<cov.length; j++) fw.write(cov[i][j]+ " ");
+        	if (i < cov.length - 1) fw.write("; ");
+        	fw.write("\n");
+        }        		
+        fw.write("]\n");
+        fw.write("Mean = [");
+        for (int i=0; i<mean.length; i++) fw.write(mean[i]+" ");
+        fw.write("]\n");
+        fw.write("@variable(m, 0 <= x[i=1:N] <= 1) \n");
+        fw.write("@objective(m,Max,sum(x[i] * Mean[i] for i = 1:N)) \n");
+        fw.write("@constraint(m, sum(x[j] * sum(x[i] * C[i,j] for i = 1:N) for j = 1:N) <= "+ min_reve +") \n");
+        fw.write("@constraint(m, sum(x[i] for i = 1:N) ==1.0) \n");
+        fw.write("status = solve(m) \n");
+        fw.write("open(\""+ SOLVER_OUTPUT + "\", \"w\") do f \n");
+        fw.write("        for i= 1:N \n");
+        fw.write("            n1 = getvalue(x[i]) \n");
+        fw.write("            write(f, \"$n1\\n\") \n");
+        fw.write("        end \n");
+        fw.write("    end \n");
+        fw.write("exit() \n");
+        fw.close();
+	}
+	
+	private static void runOsCmd(String cmd) throws IOException {
+		 Process p = Runtime.getRuntime().exec(cmd);
 		 try {
 			p.waitFor();
 		 } catch (InterruptedException e) {
 			e.printStackTrace();
 			System.exit(0);
 		 } 
+	}
+	
+	private static void runJulia(String cmd) throws IOException {
+		 try {
+			Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(cmd);
+            InputStream stdin = proc.getInputStream();
+            InputStreamReader isr = new InputStreamReader(stdin);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            while ( (line = br.readLine()) != null);
+            int exitVal = proc.waitFor();            
+		 } catch (InterruptedException e) {
+			e.printStackTrace();
+			System.exit(0);
+		 } 
+		 catch (Throwable t)
+         {
+           t.printStackTrace();
+         }
+	}
+	
+	// this will find a few outperforming stocks (max_shares) which have risks associated not greater than 'max_risk'
+	private static PortfolioWithWallet findAndBuyBestPortfolioWith_DA1(
+							LocalDate startDate, int period_length, int periods_number, BigDecimal budget, 
+							double max_risk, int max_shares, BufferedWriter bw) throws IOException {
+		double[] benefits = new double[tickers.size()]; 
+		String[] goodTickers = new String[tickers.size()];
+		Close[] lastClose = new Close[tickers.size()];
+		bw.write("<BR>New portfolio:\n<table border=1>");
+		bw.write("<TR><TH>Stock</TH><TH>W[%]</TH><TH> Vol. </TH><TH> Price</TH><TH> Total</TH></TR>\n"); 
+		int idx=0;
+		for (String symbol: tickers) {	
+			List<Close> historicPrices = prices.get(symbol);
+			List<Close> historicDividends = dividends.get(symbol);
+			Close lastPrice = null;
+			double[] dividends = new double[periods_number];
+			int i=0;
+			for (; i<periods_number; i++) {
+				LocalDate fromDate = startDate.plusMonths(i * period_length);
+				LocalDate toDate   = startDate.plusMonths((i+1) * period_length);
+
+				List<Close> historicPricesTemp = historicPrices.stream() // limiting to the "risk assessment" period
+						.filter(p -> (p.date.isAfter(fromDate) && p.date.isBefore(toDate)) || p.date.isEqual(toDate)) // stopDate belongs to the next period
+						.collect(Collectors.toList()); // maybe sorting to secure against different order in source files ?
+				
+				if (historicPricesTemp == null || historicPricesTemp.size() == 0) break; // do not take this stock into consideration
+				// summing 
+				lastPrice = historicPricesTemp.get(0); // this is called many times, but we are interested in the last one 
+				dividends[i] = historicDividends.stream() // limiting to the "risk assessment" period
+						.filter(p -> (p.date.isAfter(fromDate) && p.date.isBefore(toDate)) || p.date.isEqual(toDate))
+						.collect(Collectors.summingDouble(d -> d.price.doubleValue()));
+			}
+			if (i == periods_number) { // = we found share prices in all periods 
+				if (lastPrice == null || lastPrice.price == null || lastPrice.price.equals(0.0)) continue; // don't consider this share
+				if (StatUtils.variance(dividends) > max_risk) continue; // a too risky stock
+				benefits[idx] = StatUtils.mean(dividends) / lastPrice.price.doubleValue();
+				goodTickers[idx] = symbol;
+				lastClose[idx] = lastPrice;
+				idx++;
+			}
+		}
+		// now choosing the best ones
+		List<StockInPortfolio> portfolio = new ArrayList<StockInPortfolio>();
+		BigDecimal total = new BigDecimal(0.0);
+		double share = (1.0/(double)max_shares);
+		for (int s=0; s<max_shares; s++) {
+			// find the best 
+			List<Double> list = Arrays.stream(benefits).boxed().collect(Collectors.toList());
+			idx = list.indexOf(Collections.max(list));
+			benefits[idx] = 0.0; // don't consider this share any longer in this loop
+			// shares will be split equally
+			int volume = (int)(budget.doubleValue() * share / lastClose[idx].price.doubleValue()); 
+			portfolio.add(new StockInPortfolio(goodTickers[idx], volume));
+			bw.write("<TR><TD>"+ goodTickers[idx] +"</TD><TD>"+ String.format("%.1f",share*100.0) +"</TD><TD>"
+					   	+ volume +"</TD><TD>"+ lastClose[idx].price 
+					   	+ "</TD><TD>" + lastClose[idx].price.multiply(new BigDecimal(volume))
+					   	+ "</TD></TR>\n");
+			total = total.add(lastClose[idx].price.multiply(new BigDecimal(volume)));
+		}
+		bw.write("</table>\n");
+		bw.write("Portfolio value: " + total + "<BR>");
+		bw.write("In the wallet after stock purchase: " + String.format("%.3f",budget.subtract(total).doubleValue()) + "<BR>");
+		return new PortfolioWithWallet(budget.subtract(total).doubleValue(), portfolio);
 	}
 	
 	private static PortfolioWithWallet createPortfolio(double budget, int size, String[] goodTickers, 
@@ -256,7 +396,7 @@ public class StockSimulator {
 		double[] weights = new double[size];
 		BigDecimal total = new BigDecimal(0.0);
 		int i = 0;
-		try (BufferedReader br = new BufferedReader(new FileReader(PYTHON_OUTPUT))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(SOLVER_OUTPUT))) {
 			while (br.ready()) {
 			   String line = br.readLine();
 			   weights[i] = Double.parseDouble(line); 
@@ -284,7 +424,7 @@ public class StockSimulator {
 		bw.write("</table>\n");
 		bw.write("Portfolio value: " + total + "<BR>");
 		bw.write("In the wallet after stock purchase: " + String.format("%.3f",budget-total.doubleValue()) + "<BR>");
-		return new PortfolioWithWallet(budget-total.doubleValue(), portfolio);
+		return new PortfolioWithWallet(budget - total.doubleValue(), portfolio);
 	}
 	
 	// this function serves only as a check of Apache Commons
@@ -357,7 +497,7 @@ public class StockSimulator {
 	}
 	
 	private static List<Close> readHistory(String ticker, String suffix, int idx) {
-		List<Close> hist = new ArrayList();
+		List<Close> hist = new ArrayList<Close>();
 		try (BufferedReader br = new BufferedReader(new FileReader(ROOT_DIR + "data/"+ ticker + suffix))) {
 			br.readLine(); // ignoring the header line
 			while (br.ready()) {
@@ -374,7 +514,7 @@ public class StockSimulator {
 	}
 	
 	private static List<Stock> readStockName(String ticker, String suffix) {
-		List<Stock> hist = new ArrayList();
+		List<Stock> hist = new ArrayList<Stock>();
 		try (BufferedReader br = new BufferedReader(new FileReader(ROOT_DIR + "data/"+ ticker + suffix))) {
 			while (br.ready()) {
 				String line = br.readLine();
